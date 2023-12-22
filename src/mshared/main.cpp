d4a73fe6cfac99e2e3c6002a997402a59982968d
Jithin Renji <jithin0904@gmail.com>
2023-12-22 09:22:51 -0700

    Add ArgParser to mshared

--- src/mshared/main.cpp
+++ src/mshared/main.cpp
@@ -1,3 +1,4 @@
+#include <ArgParser.hpp>
 #include <CryptoContext.hpp>
 #include <MessageServer.hpp>
 
@@ -8,7 +9,35 @@
 #include <iostream>
 #include <string>
 
-int main() {
+void version() {
+  std::cout << "mshared v0.1\n"
+            << "Copyright (C) 2023 Jithin Renji.\n"
+            << "Free software licensed under the GNU GPLv3.\n";
+}
+
+int main(int argc, char *argv[]) {
+  MShare::OptionList options = {
+    {"help", MShare::Option::FLAG, MShare::Option::NONE,
+      "Show this help message and exit."},
+    {"version", MShare::Option::FLAG, MShare::Option::NONE,
+      "Show version information and exit."}
+  };
+
+  try {
+    MShare::ArgParser parser(argc, argv, options);
+    const MShare::ParsedOptions parsed_options = parser.get();
+    if (parsed_options.contains("help")) {
+      parser.print_help();
+      return 0;
+    } else if (parsed_options.contains("version")) {
+      version();
+      return 0;
+    }
+  } catch (MShare::UnknownOptionError& e) {
+    std::cerr << e.what() << '\n';
+    return -1;
+  }
+
   MShare::CryptoContext cc;
   std::string ciphertext = cc.encrypt("Hello, world!");
   std::string ec;
